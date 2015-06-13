8d0676cf7508d7f158433d15ce8b393f7fa8c300
Dillon Barker <dor.barker@gmail.com>
2015-06-13 10:29:19 -0600

    Fixed updating logic; added todo for seeing if an update has been requested

--- lethlabrobot.py
+++ lethlabrobot.py
@@ -1,20 +1,43 @@
 import argparse
 import nettest
 import json
+import subprocess
 from datetime import datetime
+
+def last_update_time():
+
+    proc = subprocess.Popen(["tail", "-n 1", "netspeed.log"],
+                            stdout = subprocess.PIPE,
+                            stderr = subprocess.PIPE
+                            )
+
+    proc_out, proc_err = proc.communicate()
+    l = proc_out.strip().split(',')
+
+    out = int(l[3])
+
+    return out
+
+def check_requested():
+    
+    # To do:
+    #   read most recent Twitter post
+    #   If "Update Netspeed" is in the last post, then check the time stamp
+    #   If time stamp is > last_update_time(), then return True
+    #   Else: return False
+    pass
 
 def reporter(update_hour):
 
     # update_requested = check if an update has been requested via Twitter
     # done = determine if a routine check has been performed today
     # See if the routine check has been performed by reading the logs (tail -n 1 might be easiest)
-    publish = False
-
-    if datetime.today().hour <
+    
+    done = last_update_time() >= update_hour
     routine = all([datetime.weekday(datetime.today().date()) < 5 ,datetime.today().hour == update, not done])
     
-    if update_requested or routine:
-        
+    report_net_speed(routine or update_requested)
+
 def report_net_speed(publish):
 
     speedstring = nettest.perform_speedtest()
@@ -23,7 +46,7 @@ def report_net_speed(publish):
 
     report = "Current Internet Speeds:\n" + speedstring
 
-    # if publish, then write to twitter
+    # if publish is True, then write to twitter
     
     print report # debug
     print len(report) # debug
